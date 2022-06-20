e71d9f0e903c8f675096bfa0f221ec4ce6e8b36d
Roomay <Roomaywoo@gmail.com>
2022-06-20 20:12:29 +0800

    impl deleted

--- src/main/java/com/dzqc/cloud/service/ConsultationService.java
+++ src/main/java/com/dzqc/cloud/service/ConsultationService.java
@@ -1,11 +1,19 @@
 package com.dzqc.cloud.service;
 
+import com.dzqc.cloud.dao.ConsultationMapper;
 import com.dzqc.cloud.entity.Consultation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public interface ConsultationService {
-    public List<Consultation> selectByDoctorId(int doctorId);
+public class ConsultationService {
+    @Autowired
+    private ConsultationMapper consultationMapper;
+
+    public List<Consultation> selectByDoctorId(int doctorId) {
+        return consultationMapper.selectByDoctorId(doctorId);
+    }
+
 }
